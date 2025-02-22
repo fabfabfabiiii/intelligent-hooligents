8b819f7e2a34d1c6e5d1a042ba33a642a0f71df6
Mujdrica <fabian.mujdrica@hpe.com>
2025-02-22 14:07:31 +0100

    new class person handler

--- src/main.py
+++ src/main.py
@@ -1,12 +1,5 @@
-from src.models.person import Person
-
-
 def main():
     print('Intelligent Agents')
-
-    person = Person('station', 'Neutral', 4)
-
-    print(person)
 
 if __name__ == '__main__':
     main()
--- src/models/person.py
+++ src/models/person.py
@@ -17,8 +17,23 @@ class Person:
     def __str__(self):
         return f'Ziel: {self.zielstation}, Verein: {self.verein}'
 
-    def get_current_zufriedenheit(self):
+    def get_current_zufriedenheit(self) -> int:
         return self.zufriedenheit[-1]
 
     def update_zufriedenheit(self, zufriedenheit: int):
         self.zufriedenheit.append(zufriedenheit)
+
+class PersonHandler:
+    def __init__(self):
+        self.persons: list[Person] = []
+
+    def __str__(self):
+        string: str = f'Person Handler: {len(self.persons)} Person'
+
+        if len(self.persons) != 1:
+            string += "en"
+
+        for i in range(len(self.persons)):
+            string += f'\n{(i+1)}: {self.persons[i]}'
+
+        return string
