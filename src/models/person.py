c1b679b0c85712eb66fc56f59eb9c5951ec90895
Mujdrica <fabian.mujdrica@hpe.com>
2025-02-22 11:30:46 +0100

    add constructor, new functions

--- src/models/person.py
+++ src/models/person.py
@@ -1,10 +1,14 @@
 from src.models.verein import Verein
-
+from typing import Union
 
 class Person:
-    def __init__(self, zielstation: str, verein: str, zufriedenheit: int):
+    def __init__(self, zielstation: str, verein: Union[str, Verein], zufriedenheit: int):
         self.zielstation: str = zielstation
-        self.verein: Verein = getattr(Verein, verein)
+
+        if isinstance(verein, Verein):
+            self.verein: Verein = verein
+        else:
+            self.verein: Verein = getattr(Verein, verein)
 
         #Zufriedenheit ist Liste, kriegt pro ZE neue zufriedenheit
         self.zufriedenheit: list = []
@@ -12,3 +16,9 @@ class Person:
 
     def __str__(self):
         return f'Ziel: {self.zielstation}, Verein: {self.verein}'
+
+    def get_current_zufriedenheit(self):
+        return self.zufriedenheit[-1]
+
+    def update_zufriedenheit(self, zufriedenheit: int):
+        self.zufriedenheit.append(zufriedenheit)
