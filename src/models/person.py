f9e700883383a09acc64ffdd81dcd901e10ba5e0
BuildTools <unconfigured@null.spigotmc.org>
2025-02-23 12:04:40 +0100

    add function to filter persons

--- src/models/person.py
+++ src/models/person.py
@@ -1,8 +1,9 @@
 from src.models.verein import Verein
 
 class Person:
-    def __init__(self, zielstation: str, verein: str | Verein, zufriedenheit: int):
+    def __init__(self, zielstation: str, verein: str | Verein, zufriedenheit: int, current_position: str = 'Stadion'):
         self.zielstation: str = zielstation
+        self.current_position: str = current_position
 
         if isinstance(verein, Verein):
             self.verein: Verein = verein
@@ -22,6 +23,9 @@ class Person:
     def update_zufriedenheit(self, zufriedenheit: int):
         self.zufriedenheit.append(zufriedenheit)
 
+    def has_arrived(self) -> bool:
+        return self.current_position == self.zielstation
+
 class PersonHandler:
     def __init__(self):
         self.persons: list[Person] = []
@@ -36,3 +40,15 @@ class PersonHandler:
             string += f'\n{(i+1)}: {self.persons[i]}'
 
         return string
+
+    def add_person(self, person: Person):
+        self.persons.append(person)
+
+    def get_persons_at_location(self, location: str) -> list[Person]:
+        persons_at_station: list[Person] = []
+
+        for person in self.persons:
+            if person.current_position == location:
+                persons_at_station.append(person)
+
+        return persons_at_station
