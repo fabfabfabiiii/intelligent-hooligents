bce410799346eeec7fd73ccc53f9baa6633c1bd3
BuildTools <unconfigured@null.spigotmc.org>
2025-02-24 11:53:50 +0100

    add config parameters for graph visualization

--- src/config.py
+++ src/config.py
@@ -1,3 +1,8 @@
 GRAPHML_PATH: str = "../resources/Verkehrsnetz.graphml"
 USE_SEED: bool = True
 SEED: int = 127456981
+#Graph Visualization
+NODE_COLOR: str = 'red'
+NODE_COLOR_HIGHLIGHTED: str = 'green'
+EDGE_COLOR: str = 'blue'
+EDGE_COLOR_HIGHLIGHTED: str = 'yellow'
