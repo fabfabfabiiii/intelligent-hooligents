244ad777b3ce034a4b8b66186447187208fbe0e3
Mujdrica <fabian.mujdrica@hpe.com>
2025-04-09 09:50:55 +0200

    add constructor

--- src/models/impl/ImplRouteCalculator.py
+++ src/models/impl/ImplRouteCalculator.py
@@ -2,11 +2,12 @@ from models.abstract.route_calculator import RouteCalculator
 from models.streckennetz import Streckennetz
 from networkx import Graph
 
-
-
 class ImplRouteCalculator(RouteCalculator):
     def __init__(self, graph: Streckennetz | Graph):
-        pass
+        if isinstance(graph, Streckennetz):
+            self.graph: Streckennetz = graph
+        else:
+            self.graph: Streckennetz = Streckennetz.from_graph(graph, False)
 
     def calculate_route(self, start_node: str,
                         mandatory_nodes: list[str] | None) -> list[str]:
