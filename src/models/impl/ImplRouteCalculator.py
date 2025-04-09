a995e25071e9325889baba9c395ff2f0a2de9b4e
Mujdrica <fabian.mujdrica@hpe.com>
2025-04-09 10:18:24 +0200

    fix bug

--- src/models/impl/ImplRouteCalculator.py
+++ src/models/impl/ImplRouteCalculator.py
@@ -11,7 +11,7 @@ class ImplRouteCalculator(RouteCalculator):
         if isinstance(graph, Streckennetz):
             self.graph: Streckennetz = graph
         else:
-            self.graph: Streckennetz = Streckennetz.from_graph(graph, False)
+            self.graph: Streckennetz = Streckennetz.from_nx_graph(graph, False)
 
     def calculate_route(self, start_node: str,
                         mandatory_nodes: list[str] | None) -> list[str]:
